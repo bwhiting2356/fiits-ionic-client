116788e6bcfebfd3a4905710d116ef60b6951b5b
Brendan Whiting <brendan.whiting@cognizant.com>
2019-11-14 15:45:47 -0800

    coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('@angular/cli/plugins/karma'),
+      require('karma-coverage')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
